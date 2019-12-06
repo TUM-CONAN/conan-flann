783c2e279bae0179ba31c93409101deaec30f3bb
ulrich eck <ulrich.eck@tum.de>
2019-12-06 20:27:40 +0100

    upgraded ircad_common

--- conanfile.py
+++ conanfile.py
@@ -29,7 +29,7 @@ class LibFlannConan(ConanFile):
         del self.settings.compiler.libcxx
 
     def requirements(self):
-        self.requires("ircad_common/1.0.0@camposs/stable")
+        self.requires("ircad_common/1.0.2@camposs/stable")
 
     def source(self):
         tools.get("https://github.com/mariusmuja/flann/archive/{0}.tar.gz".format(self.upstream_version))
